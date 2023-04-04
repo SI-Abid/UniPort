55878e7c35b5176f46438bd18d658183b3de09c9
SI-Abid <siaabid5323@gmail.com>
2023-04-05 02:10:55 +0600

    pushed with 2 feature

--- lib/version_1/constants/secret.dart
+++
@@ -1,7 +0,0 @@
-class Secret {
-  static String server = 'https://web.uniport.up.railway.app';
-  static String serverKey = '68Cfak';
-  static String pushKey =
-      'key=AAAAaOpaTeI:APA91bF5wE5xHyyIva5D8q6OFIkj3qgJFmK6jfbR5CF94XGQhdmYYlpH0nIPpLoLsPiXXxnwb6oXUd8agh4FbelL0WgoV0ym1kZEAkfPRvYWFTVaxfiUNIiiO-Uewbb8BjXQMHP5eqWT';
-  static String pushUrl = 'https://fcm.googleapis.com/fcm/send';
-}
